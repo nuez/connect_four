c9fdba3589e37e47dcfc3de9dd6df58cd9454491
nuez <teun@nuezweb.com>
2016-11-06 14:03:51 +0100

    Add Tests scaffolding

---
+++ tests/src/Unit/ConnectFourServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\connect_four\Tests\ConnectFourServiceTests
+ */
+
+namespace Drupal\Tests\connect_four\Unit\ConnectFourServiceTests;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\connect_four\ConnectFourService
+ *
+ * @group connect_four
+ */
+class ConnectFourServiceTests extends UnitTestCase  {
+
+
+  public function setUp() {
+    parent::setUp();
+  }
+
+}
